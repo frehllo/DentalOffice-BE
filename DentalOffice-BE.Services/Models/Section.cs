8d8bf6980169ef4e3d2533af206702728e9823b1
frehllo <narcisbuzatu2003@gmail.com>
2024-03-09 12:46:36 +0100

    added some of controllers

---
+++ DentalOffice-BE.Services/Models/Section.cs
@@ -0,0 +1,37 @@
+﻿using DentalOffice_BE.Common;
+using DentalOffice_BE.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalOffice_BE.Services.Models;
+
+public class Section
+{
+    public long? SectionId { get; set; }
+    public string Title { get; set; } = null!;
+    public string Route { get; set; } = null!;
+    public string? ApiString { get; set; }
+    public SectionConfiguration? Configuration { get; set; }
+}
+
+public class SectionViewModel
+{
+
+    public long Id { get; set; }
+    public SectionDto? Section { get; set; }
+    public ICollection<SectionDto>? SubSections { get; set; }
+    public DateTime? InsertDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+}
+
+public class SectionUpdateModel
+{
+    public long Id { get; set; }
+}
+
+public class SectionInsertModel
+{
+}
---
+++ DentalOffice-BE/Controllers/SectionController.cs
@@ -0,0 +1,16 @@
+﻿using DentalOffice_BE.Services.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace DentalOffice_BE.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SectionController : ControllerBase
+{
+    [HttpGet]
+    public async IEnumerable<SectionViewModel> GetList()
+    {
+
+    }
+}
