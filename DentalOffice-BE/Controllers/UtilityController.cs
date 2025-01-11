1c88ecd37ea10eae535353d7b964ac3022c2e8da
frehllo <narcisbuzatu2003@gmail.com>
2025-01-11 11:46:41 +0100

    optimization

---
+++ DentalOffice-BE/Controllers/UtilityController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DentalOffice_BE.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/")]
+    public class UtilityController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { status = "Server is up and running!" });
+        }
+    }
+}
